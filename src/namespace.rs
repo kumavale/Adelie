b388f86703599cc41d6e5c4e94136046fdc0dabb
kumavale <kumavale.hyperion@gmail.com>
2022-11-26 15:33:24 +0900

    WIP (コンパイル及びテストは通過済み)

--- src/namespace.rs
+++ src/namespace.rs
@@ -1,39 +1,70 @@
-use std::rc::Rc;
+use std::borrow::BorrowMut;
+use std::cell::RefCell;
+use std::rc::{Rc, Weak};
 use super::function::*;
 
-#[derive(Clone, Debug, PartialEq)]
+/// NameSpace {
+///     name: crate,
+///     parent: None
+///     children: [
+///         NameSpace {
+///             name: module1,
+///             parent: Some(NameSpace { name: crate, ... } ),
+///             children: [],
+///             elements: [
+///                 fn bar();
+///             ],
+///         }
+///     ],
+///     elements: [
+///         fn main();
+///         fn foo();
+///     ],
+/// }
+#[derive(Clone, Debug)]
 pub struct NameSpace<'a> {
     pub name: String,
+    pub parent: RefCell<Weak<NameSpace<'a>>>,
+    //pub children: RefCell<Vec<Rc<NameSpace<'a>>>>,
+    //pub children: RefCell<Vec<NameSpace<'a>>>,
     pub children: Vec<NameSpace<'a>>,
     pub elements: Vec<Rc<Function<'a>>>,
 }
 
 impl<'a> NameSpace<'a> {
-    pub fn new(name: &str) -> Self {
+    pub fn new(name: &str, parent: Option<Rc<NameSpace<'a>>>) -> Self {
         NameSpace {
             name: name.to_string(),
+            parent: RefCell::new(Weak::new()),
+            //parent: if let Some(parent) = parent {
+            //    Rc::downgrade(&parent)
+            //} else {
+            //    Weak::new()
+            //},
+            //children: RefCell::new(vec![]),
             children: vec![],
             elements: vec![],
         }
     }
 
     pub fn find_mut(&mut self, name: &str) -> Option<&mut Self> {
-        self.children
-            .iter_mut()
-            .find(|n| n.name == name)
+        // TODO: 処理が間違っている気がする
+         self.children
+             //.borrow_mut()
+             .iter_mut()
+             .find(|ns| ns.name == name)
+             //.map(|ns|Rc::<NameSpace<'a>>::get_mut(ns).unwrap())
     }
 
     pub fn find_mut_recursive(&mut self, name: &str) -> Option<&mut Self> {
-        if self.name == name {
-            Some(self)
-        } else {
-            self.children
-                .iter_mut()
-                .find_map(|n| n.find_mut_recursive(name))
-        }
-    }
-
-    pub fn append_child(&mut self, child: Self) {
-        self.children.push(child);
+        //1 if self.name == name {
+        //1     Some(self)
+        //1 } else {
+        //1     self.children
+        //1         .borrow_mut()
+        //1         .iter_mut()
+        //1         .find_map(|n| n.find_mut_recursive(name))
+        //1 }
+        todo!()
     }
 }
--- src/program.rs
+++ src/program.rs
@@ -1,4 +1,6 @@
 use std::borrow::Borrow;
+use std::borrow::BorrowMut;
+use std::cell::RefCell;
 use std::rc::Rc;
 use super::class::*;
 use super::function::*;
@@ -13,7 +15,11 @@ pub struct Program<'a> {
     pub functions: Vec<Rc<Function<'a>>>,
     pub structs: Vec<Struct>,
     pub impls: Vec<Impl<'a>>,
-    pub namespace: NameSpace<'a>,
+    //pub namespace: NameSpace<'a>,
+    pub namespace: Rc<NameSpace<'a>>,
+    //pub namespace: Rc<RefCell<NameSpace<'a>>>,
+    //pub namespace: RefCell<Rc<NameSpace<'a>>>,
+    //pub namespace: RefCell<NameSpace<'a>>,
 }
 
 impl<'a> Program<'a> {
@@ -24,7 +30,11 @@ impl<'a> Program<'a> {
             functions: vec![],
             structs: vec![],
             impls: vec![],
-            namespace: NameSpace::new("crate"),
+            //namespace: NameSpace::new("crate", None),
+            namespace: Rc::new(NameSpace::new("crate", None)),
+            //namespace: Rc::new(RefCell::new(NameSpace::new("crate", None))),
+            //namespace: RefCell::new(Rc::new(NameSpace::new("crate", None))),
+            //namespace: RefCell::new(NameSpace::new("crate", None)),
         }
     }
 
@@ -45,7 +55,7 @@ impl<'a> Program<'a> {
             panic!("the name `{}` is defined multiple times", f.name);
         } else {
             let f = Rc::new(f);
-            self.namespace.elements.push(Rc::clone(&f));
+            Rc::make_mut(&mut self.namespace).elements.push(Rc::clone(&f));
             self.functions.push(f);
         }
     }
@@ -59,23 +69,39 @@ impl<'a> Program<'a> {
     }
 
     pub fn push_or_merge_impl(&mut self, mut i: Impl<'a>) {
+        // TODO: たぶん処理が間違っている
         if let Some(dst) = self.impls.find_mut(&i.name) {
-            if let Some(ns) = self.namespace.find_mut(&i.name) {
+            if let Some(ns) = Rc::make_mut(&mut self.namespace).find_mut(&i.name) {
                 ns.elements.extend_from_slice(&i.functions);
                 dst.functions.append(&mut i.functions);
             } else {
                 unreachable!()
             }
         } else {
-            let mut ns = NameSpace::new(&i.name);
+            let mut ns = NameSpace::new(&i.name, None);
             ns.elements = i.functions.clone();
-            self.namespace.children.push(ns);
+            //self.namespace.borrow_mut().children.borrow_mut().push(Rc::new(ns));
+            //self.namespace.borrow_mut().children.borrow_mut().push(ns);
+            //self.namespace.borrow_mut().children.push(ns);
+            Rc::make_mut(&mut self.namespace).children.push(ns);
             self.impls.push(i);
         }
     }
 
     pub fn enter_namespace(&mut self, id: &str) {
-        self.namespace.append_child(NameSpace::new(id));
+        //let child = NameSpace::new(id, Some(Rc::clone(&self.namespace.borrow())));
+        //let child = NameSpace::new(id, Some(self.namespace.clone().borrow_mut()));
+        //let namespace = Rc::new(&self.namespace.borrow_mut());
+        let child = NameSpace {
+            name: id.to_string(),
+            parent: RefCell::new(Rc::downgrade(&self.namespace.borrow_mut())),
+            //children: RefCell::new(vec![]),
+            children: vec![],
+            elements: vec![],
+        };
+        //self.namespace.borrow_mut().children.borrow_mut().push(child);
+        //self.namespace.borrow_mut().children.push(child);
+        Rc::make_mut(&mut self.namespace).children.push(child);
     }
 
     pub fn leave_namespace(&mut self) {
