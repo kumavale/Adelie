e53b9580ddb047a8b5ef0557dd2559f2b6952543
kumavale <kumavale.hyperion@gmail.com>
2023-01-18 21:39:19 +0900

    代入文の型推論のバグを修正

--- src/typing.rs
+++ src/typing.rs
@@ -628,6 +628,7 @@ fn typing_assign<'a>(current_token: &[Token], st: &mut SymbolTable, p: &'a Progr
             }
             let mut rty = typing(rhs, st, p)?;
             type_inference(&obj.borrow().ty, &mut rty);
+            type_inference(&rty, &mut obj.borrow_mut().ty);
             let is_assigned = obj.borrow().is_assigned();
             obj.borrow_mut().assigned = true;
 
